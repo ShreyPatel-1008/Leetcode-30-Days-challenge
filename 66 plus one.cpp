22ed972b5763d6631e03e3ee56ddb0e9d07d9060
Elite_Shrey <shreypatel4307@gmail.com>
2025-12-05 23:04:52 +0530

    Add plusOne function to increment digits
    Implement a solution to increment a number represented as an array of digits.

---
+++ 66 plus one.cpp
@@ -0,0 +1,22 @@
+//66.plus one
+//time complexity: O(n)
+//space complexity: O(1)
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+         for (int i = digits.size() - 1; i >= 0; i--) {
+            
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+           
+            digits[i] = 0;
+        }
+
+        
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
