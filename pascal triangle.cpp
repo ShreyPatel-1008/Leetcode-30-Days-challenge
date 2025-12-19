77839b77a7f7ce07de0bead12f4901f93b277a9a
Elite_Shrey <shreypatel4307@gmail.com>
2025-12-19 23:51:05 +0530

    Add solution for generating Pascal's Triangle
    Implement Pascal's Triangle generation.

---
+++ pascal triangle.cpp
@@ -0,0 +1,22 @@
+//118. Pascal Triangle
+//https://leetcode.com/problems/pascals-triangle/
+//time complexity: O(numRows^2)
+//space complexity: O(1)
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < numRows; i++) {
+            vector<int> row(i + 1, 1);  
+
+            for (int j = 1; j < i; j++) {
+                row[j] = ans[i - 1][j - 1] + ans[i - 1][j];
+            }
+
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
